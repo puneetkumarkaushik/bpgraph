e90968f21ee1bf123eb82dc7244f458050bff700
puneetkumarkaushik <mech.pkkaushik@gmail.com>
2020-01-24 21:43:59 +0530

    Updates

---
+++ precache-manifest.05b72586c807770414cc4181425949b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d34a3d56133da6b519e237b2e3a7aaaa",
+    "url": "/bp-readings/index.html"
+  },
+  {
+    "revision": "dddd8023bdcfde532c2c",
+    "url": "/bp-readings/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "84b76306c5d974a04e95",
+    "url": "/bp-readings/static/css/main.fe426c93.chunk.css"
+  },
+  {
+    "revision": "dddd8023bdcfde532c2c",
+    "url": "/bp-readings/static/js/2.3a77c844.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/bp-readings/static/js/2.3a77c844.chunk.js.LICENSE"
+  },
+  {
+    "revision": "84b76306c5d974a04e95",
+    "url": "/bp-readings/static/js/main.86221dbd.chunk.js"
+  },
+  {
+    "revision": "ede5977f5045878ab33b",
+    "url": "/bp-readings/static/js/runtime-main.46f167f4.js"
+  }
+]);
